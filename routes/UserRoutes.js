b145dc004118d0a9af7c0d37203c0f3d4aae9f17
Vrutik Sidapara <work.vrutik.s@gmail.com>
2026-01-01 17:56:04 +0530

    chore: remove unused comments from user routes

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,19 +3,10 @@ const router = express.Router();
 
 const userController = require("../controllers/UserController");
 
-// CREATE user
 router.post("/", userController.createUser);
-
-// GET all users
 router.get("/", userController.getAllUsers);
-
-// GET user by ID
 router.get("/:id", userController.getUserById);
-
-// UPDATE user
 router.put("/:id", userController.updateUser);
-
-// DELETE user
 router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
